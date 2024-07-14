da9dc51716b56743d31ac7e39aa5aad9072720c3
Henry Owens <henryowens@Henrys-MacBook-Pro.local>
2024-07-14 18:18:15 +0200

    Adding meta info & favicon

--- app.vue
+++ app.vue
@@ -6,6 +6,31 @@
   </div>
 </template>
 
+
+<script setup lang="ts">
+const SEO_META = {
+  title: 'Henry Owens',
+  description: 'Henry Owens is a software engineer with a passion for web development and design. He is a full-stack developer with experience in front-end and back-end technologies.',
+  image: 'https://henryowens.dev/og-image.png'
+}
+
+const route = useRoute()
+
+
+useSeoMeta({
+  title: SEO_META.title,
+  description: SEO_META.description,
+  ogTitle: SEO_META.title,
+  ogDescription: SEO_META.description,
+  ogImage: SEO_META.image,
+  ogUrl: () => `https://henryowens.dev/${route.path}`,
+  ogType: 'website',
+  twitterTitle: SEO_META.title,
+  twitterDescription: SEO_META.description,
+  twitterImage: SEO_META.image,
+})
+</script>
+
 <style scoped lang="scss">
 .app {
   @apply min-h-screen;
